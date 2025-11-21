02c77e5e544fc69b35f6968f7450481bdb416da8
Lê Trung Thắng <mac@MacBookPro.lan>
2025-11-21 23:12:37 +0700

    first commit

---
+++ buoi_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+      int a, b;
+      printf("nhap a: ");
+      scanf("%d",&a);
+      printf("nhap b: ");
+      scanf("%d", &b);
+      printf("tong a và b là: %d", a + b);
+      return 0;
+}
